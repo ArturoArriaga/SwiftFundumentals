96d4fd7b579ac9ca1aae52675f786ecc9fc4c9eb
ArturoArriaga <32577651+ArturoArriaga@users.noreply.github.com>
2022-06-08 14:30:39 -0500

    Intro To Closures

--- Intro To Functional Swift.playground/Pages/Closures.xcplaygroundpage/Contents.swift
+++ Intro To Functional Swift.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -45,3 +45,43 @@ var addClosuresThree: (Int, Int) -> Int = {
 var addClosuresFour: (Int, Int) -> Int = {$0 + $1}
 addClosuresFour(4,5)
 // here swift infers that $0 is 4 and $1 is 5
+
+// First Class Citizen
+// In Swift, functions and closures are referred to as "First Class Citizens". This means that fuctions can be: stored in a variable/constant; be passed as a parameter; return.
+
+// Return Closure: Lets create a function that returns a closure block.
+// Option 1: Return Closure Indirectly
+func returnClosure() -> ((Int, Int) -> Int) {
+    return addClosures
+}
+let addCourse = returnClosure()
+addCourse(4,10)
+returnClosure()(4,10)
+
+// Option 2: Return Closure Directly
+func returnClosureDirectly() -> ((Int, Int) -> Int) {
+    return { (num1, num2) in num1 + num2}
+}
+returnClosureDirectly()(10,10)
+
+// Or we can use the shortened form.
+func returnClosureDirectlyTwo() -> ((Int, Int) -> Int) {
+    return {$0 + $1}
+}
+returnClosureDirectlyTwo()(100,100)
+
+
+// Pass Closure: Create a function whose parameter accepts () -> String. Then, execute the closure block within the function.
+func insertClosureBlock(closureBlock: () -> String) -> String {
+    return closureBlock()
+}
+// Design ClosureBlock(): Create a closure that can be passed into the function above.
+func hello() -> String {
+    return "Hello World"
+}
+
+// Pass Closure Indirectly
+insertClosureBlock(closureBlock: hello)
+// Pass Closure Directly
+insertClosureBlock(closureBlock: { return "Hello Everyone!"})
+insertClosureBlock(closureBlock: {"Hi, I'm here :)"})
