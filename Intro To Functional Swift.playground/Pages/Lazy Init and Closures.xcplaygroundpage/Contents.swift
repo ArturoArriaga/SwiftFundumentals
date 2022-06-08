f0522095ef7b9deff8850a2c27137edc217bc37e
ArturoArriaga <32577651+ArturoArriaga@users.noreply.github.com>
2022-06-08 14:41:01 -0500

    Lazy Init and Closures

--- Intro To Functional Swift.playground/Pages/Lazy Init and Closures.xcplaygroundpage/Contents.swift
+++ Intro To Functional Swift.playground/Pages/Lazy Init and Closures.xcplaygroundpage/Contents.swift
@@ -2,6 +2,34 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+// Lazy Initialization and Closures
 
-//: [Next](@next)
+// Create an Object with Closure
+struct Human {
+    init() {
+        print("Born in 1987")
+    }
+}
+
+// Initializing in Single Line
+let art = { () -> Human  in
+    let human = Human()
+    return human
+}()
+
+// Now, art contains a human object
+
+// Introducing Lazy Var
+class IntenseMathProblem {
+    lazy var complexNumber: Int = {
+        1 * 10
+    }()
+}
+// lazy properties are not initialized until you access them.
+let problem = IntenseMathProblem() // no value for complexNumber
+problem.complexNumber // complexNumber is now 10
+
+// Rules:
+// 1. Lazy can't be used with let, since there's no initial value.
+// 2. Lazy can't be used with computed properties, since a CP is always recalculated.
+// 3. Lazy is only valid for members of a struct or class. 
